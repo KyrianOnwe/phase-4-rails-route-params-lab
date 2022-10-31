9356084543d6bf52a79d583791820e90db1123e7
Kyrian <dr.kyrian.onwe@gmail.com>
2022-10-31 00:10:52 -0500

    commit tree 2be7b32a7ecc052724c71cc08e3700de8dd2a579

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
-  def index
-    students = Student.all
-    render json: students
+  def index(*arg)
+    # students = Student.all
+    # render json: students
+    # find = Student.find_by(first_name: params[:word] || last_name: params[:word])
+    if arg.length == 0
+      students = Student.all
+      render json: students      
+    else
+      student = Student.find_by(first_name: params[:arg] || last_name: params[:arg])
+      render json: student      
+    end    
+  end
+
+  def which
+    student =  Student.find(params[:id])
+    render json: student
   end
 
 end
